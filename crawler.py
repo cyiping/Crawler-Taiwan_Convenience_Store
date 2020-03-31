7b712149bf6a01f8a34d1d2d4d991524a78a02ad
cyiping <yiping.cheng@cyiping.com>
2020-03-31 17:09:11 +0800

    2020/03/31 17:09

--- crawler.py
+++ crawler.py
@@ -1,18 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 31 14:18:23 2020
 
-@author: usem
+* Parse Data Table
+https://covid19info.live/zh/
+
+* BeautifulSoup parse table
+https://leemeng.tw/beautifulsoup-cheat-sheet.html
+
+
+
 """
 import requests
-import bs4 as BeautifulSoup
+from bs4 import BeautifulSoup
 import json
 
 url1 = 'https://data.covid19info.live/processeddata.js'
+
+'''
 a = requests.get(url1).json()
 b = json.dumps(a)
 f=open('data.txt','w')
 f.write(b)
 f.close()
 
-print(a)
+'''
+
+
+
+url2 = "https://data.covid19info.live"
+html2 = requests.get(url2)
+sp2 = BeautifulSoup(html2.text, "html.parser")
+st = sp2.find("table", {"class", })
+print(st)
